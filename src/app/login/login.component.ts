c82736292ef6f96e0323329b2b2d283db0cf12a5
Gustavo Gonçalves <gustavogonsalves2500@gmail.com>
2020-06-27 11:33:12 -0300

    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+
+@Component({
+    selector: "Login",
+    templateUrl: "./login.component.html"
+})
+export class LoginComponent implements OnInit {
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
